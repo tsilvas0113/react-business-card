6042828f31efac5aefda0b53909bc4ca73456d0f
Tristan <tsilvas0113@gmail.com>
2022-06-14 14:38:02 -0500

    Update and add styling to info section

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -1,8 +1,20 @@
 import React from "react";
+import pic from '../113.png'
 
 function Info () {
     return (
-        <h1>This is the Info Section</h1>
+        <div className="info-section">
+            <img className="info-pic" src={pic} alt='profile-picture' />
+            <div className="info-box">
+                <div className="name">Tristan Silvas</div>
+                <div className="job">Frontend Developer</div>
+                <div className="site">tristansilvas.website</div>
+                <div className="info-btns">
+                    <button className="email btn"><span className="email-icon"></span>Email</button>
+                    <button className="linkedin btn"><span className="linkedin-icon"></span>LinkedIn</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
