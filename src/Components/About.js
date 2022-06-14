49641739a2dda1f66449e18884b0d3d3096cd1f5
Tristan <tsilvas0113@gmail.com>
2022-06-14 17:32:18 -0500

    Update and style 'about' section

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function About () {
     return (
-        <h1>This is the About Section</h1>
+        <div className="about-container">
+            <div className="about-head">About</div>
+            <div className="about-text">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</div>
+        </div>
     )
 }
 
